c297424f66a48b0b944b7d4b1270fae6aa80668a
cre4mil <panisarakunkam@gmail.com>
2025-02-07 00:08:17 +0700

    ✨ feat(quiz.js): ดึงองค์ประกอบ DOM และกำหนดค่าในตัวแปร

---
+++ quiz.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const quizContainer = document.getElementById("quiz-container");
+    const resultContainer = document.getElementById("result-container");
+    const startBtn = document.getElementById("start-btn");
+    const submitBtn = document.getElementById("submit-btn");
+    const restartBtn = document.getElementById("restart-btn");
+    const timerDisplay = document.getElementById("timer");
+    const questionContainer = document.getElementById("question-container");
+    const scoreDisplay = document.getElementById("score");
+    const answersDisplay = document.getElementById("answers");
+});
